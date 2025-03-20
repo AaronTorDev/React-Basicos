8e2f318cc2de4995cc0ddef74f23d678bec253d4
Aaron Tordel <aarontordel1@gmail.com>
2025-03-19 20:38:04 -0600

    11. Eventos Nativos

--- src/components/Eventos.js
+++ src/components/Eventos.js
@@ -1,6 +1,6 @@
 import React, {Component} from "react";
 
-class Eventos extends Component {
+export class EventosES6 extends Component {
     constructor(props){
         super(props);
         this.state = {
@@ -33,7 +33,7 @@ class Eventos extends Component {
     render(e) { 
         return (
             <div>
-                <h2>Eventos En Componentes de CLase</h2>
+                <h2>Eventos En Componentes de CLase ES6</h2>
                 <nav>
                     <button onClick={this.sumar}>+</button>
                     <button onClick={this.restar}>-</button>
@@ -47,5 +47,83 @@ class Eventos extends Component {
 
 }
 
- 
-export default Eventos;
+//Properties Initializer
+
+export class EventosES7 extends Component {
+
+        state = {
+            contador: 0,
+        };
+
+
+    sumar = (e) => {
+        console.log("Sumando");
+        console.log(this);
+
+        this.setState({
+            contador: this.state.contador + 1,
+        });
+    };
+
+    restar = (e) => {
+        console.log("Restando");
+        console.log(this);
+
+        this.setState({
+            contador: this.state.contador - 1,
+        });
+    };
+    
+
+    render() { 
+        return (
+            <div>
+                <h2>Eventos En Componentes de CLase ES7</h2>
+                <nav>
+                    <button onClick={this.sumar}>+</button>
+                    <button onClick={this.restar}>-</button>
+                </nav>
+                <h3>{this.state.contador}</h3>
+            </div>
+        );
+
+    }
+
+
+}
+
+// function Boton(props){
+//     return(
+//         <button onClick = {props.myOnClick} >Boton Hecho Componente</button>
+//     )
+// }
+
+
+
+// const Boton = (props) => <button onClick = {props.myOnClick} >Boton Hecho Componente</button>
+const Boton = ({myOnClick}) => <button onClick = {myOnClick}>Boton Hecho Componente</button>
+
+
+
+export class MasSobreEventos extends Component {
+
+    handleClick = (e, mensaje) => {
+        console.log(e);
+        console.log(e.target);
+        console.log(e.nativeEvent);
+        console.log(e.nativeEvent.target);
+        console.log(mensaje)
+    }
+
+    render(){
+        return(
+            <div>
+                <h2>Mas Sobre Eventos</h2>
+                <button onClick = {(e) => this.handleClick(e, "Hola pasando parametro desde un evento")} >Saludar</button>
+                {/* Evento Personalizado */}
+                {/* <Boton onClick = {(e) => this.handleClick(e, "Hola pasando parametro desde un evento")} /> */}
+            <Boton myOnClick = {(e) => this.handleClick(e, "Hola pasando parametro desde un evento")}/>
+            </div>
+        )
+    }
+}
